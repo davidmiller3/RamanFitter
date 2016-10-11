b4e0a2f8ea18adac567c288404db4566d15293a4
davidmiller3 <dmiller3@uoregon.edu>
2016-10-10 17:25:35 -0700

    1010

--- helperFiles.py
+++ helperFiles.py
@@ -17,7 +17,7 @@ import numpy as np
 import pandas as pd
 import math
 import os
-from lmfit.models import GaussianModel, LinearModel
+from lmfit.models import GaussianModel, LinearModel, LorentzianModel
 import lmfit
 from peak_Detect import peakdetect
 def doubleGaussian(x,y,FWHM1, FWHM2, loc1,loc2,h1,h2):
@@ -63,7 +63,51 @@ def doubleGaussian(x,y,FWHM1, FWHM2, loc1,loc2,h1,h2):
     """Returns output fit as will as list of important fitting parameters"""
     return out, [fittedCenter1, fittedAmp1, fittedsigma1], [fittedCenter2, fittedAmp2, fittedsigma2]
     
+def doubleLorentizan(x,y,FWHM1, FWHM2, loc1,loc2,h1,h2):
+    sig1=FWHM1/2
+    sig2=FWHM2/2
+    amp1=h1*np.pi
+    amp2=h2*np.pi
+#Define a linear model and a Damped Oscillator Model    
+    line_mod = LinearModel(prefix='line_')
+    g1_mod = LorentzianModel(prefix='g1_')
+    g2_mod = LorentzianModel(prefix='g2_')
+#Initial Pars for Linear Model
+    pars =  line_mod.make_params(intercept=0, slope=0)
+    pars['line_intercept'].set(0, vary=True)
+    pars['line_slope'].set(0, vary=True)
+    pars.update(g1_mod.make_params())
+    pars.update(g2_mod.make_params())
+#Extend param list to use multiple peaks. Currently unused.
+#Add fit parameters, Center, Amplitude, and Sigma
+    pars['g1'+'_center'].set(loc1,min=375, max=390)
+    pars['g1'+'_sigma'].set(sig1,min=2, max=25)
+    pars['g1'+'_amplitude'].set(amp1)    
+    pars['g2'+'_center'].set(loc2,min=400, max=415)
+    pars['g2'+'_sigma'].set(sig2,min=2, max=25)
+    pars['g2'+'_amplitude'].set(amp2)    
+#Create full model. Add linear model and all peaks
+    mod=line_mod+g1_mod+g2_mod
+#Initialize fit
+    init = mod.eval(pars, x=x)
+#Do the fit. The weight exponential can weight the points porportional to the
+#amplitude of y point. In this way, points on peak can be given more weight.     
+    out=mod.fit(y, pars,x=x)
+#Get the fit parameters
+    fittedsigma1 = out.params['g1_sigma'].value
+    fittedAmp1 = out.params['g1_amplitude'].value
+    fittedCenter1 = out.params['g1_center'].value
+    fittedsigma2 = out.params['g2_sigma'].value
+    fittedAmp2 = out.params['g2_amplitude'].value
+    fittedCenter2 = out.params['g2_center'].value
+    fittedIntercept = out.params['line_intercept'].value
+    fittedSlope = out.params['line_slope'].value
+#Returns the output fit as well as an array of the fit parameters
+    """Returns output fit as will as list of important fitting parameters"""
+    return out, [fittedCenter1, fittedAmp1, fittedsigma1], [fittedCenter2, fittedAmp2, fittedsigma2]
     
+    
+        
     
 
 class ramanScan():
@@ -74,16 +118,33 @@ class ramanScan():
         self.ypos=pdarray.Y.max()
         self.wnarray=pdarray.wavenumber
         self.iarray = pdarray.Intensity
-    def fitMoS2Peaks(self):
+    def fitMoS2PeaksG(self):
         low = 350
         high = 425
         smalldf = self.pdarray[self.pdarray.wavenumber<high][self.pdarray.wavenumber>low]
-        if len(peakdetect(smalldf.Intensity, smalldf.wavenumber, lookahead = 2, delta=np.abs(smalldf.Intensity.min())*1.1)[0])==0:
+        try:
+            if len(peakdetect(smalldf.Intensity, smalldf.wavenumber, lookahead = 2, delta=np.absolute(smalldf.Intensity.mean())*1.1)[0])==0:
+                self.noFit = True
+            else:
+                self.out,self.g1,self.g2 = doubleGaussian(smalldf.wavenumber,smalldf.Intensity,20,20,385,402,0,0)
+                self.noFit=False
+        except:
             self.noFit = True
-        else:
-            self.out,self.g1,self.g2 = doubleGaussian(smalldf.wavenumber,smalldf.Intensity,20,20,385,402,0,0)
-            self.noFit=False
-        print 'fitted'
 
-
+    def fitMoS2PeaksL(self):
+        low = 350
+        high = 425
+        smalldf = self.pdarray[self.pdarray.wavenumber<high][self.pdarray.wavenumber>low]
+        try:
+            if len(peakdetect(smalldf.Intensity, smalldf.wavenumber, lookahead = 2, delta=np.absolute(smalldf.Intensity.mean())*1.1)[0])==0:
+                self.noFit = True
+                print 'no'
+            else:
+                self.out,self.g1,self.g2 = doubleLorentizan(smalldf.wavenumber,smalldf.Intensity,20,20,385,402,0,0)
+                self.noFit=False
+                print 'yes'
+        except:
+            self.noFit = True
+            print 'no'
+            
 
--- mainScripts.py
+++ mainScripts.py
@@ -16,23 +16,31 @@ import seaborn as sns
 from peak_Detect import peakdetect
 #import lmPeakFit as lpf
 import lmfit
-path = "C:\\Users\\David\\Desktop\\MoS2_SlideA1\\Run004Map.txt"
-def importRamanMap(pathToMapFile):
+path = "Z:\\Group\\Projects\\MoS2\\Raman Spectra\\Slide01\\MoS2_SlideA1\\Run004Map.txt"
+path2 = "Z:\\Group\\Projects\\MoS2\\Raman Spectra\\DavidMiller\\MoS2_SlideA2\\map2.txt"
+def importRamanMap(pathToMapFile, fitType='G'):
     rawdata = pd.read_csv(pathToMapFile, sep='\t',skiprows=1, names = ['X','Y','wavenumber','Intensity'])
     x = rawdata
     xu= x.X.unique()
     yu= x.Y.unique()
     runList=[]
+    print rawdata.Intensity.min()
+    num=0
     for i in xu:
         for ii in yu:
+            print num
             runList.append(ramanScan(x[x.X==i][x.Y==ii].sort('wavenumber')))
-            runList[-1].fitMoS2Peaks()
+            if fitType == 'L':
+                runList[-1].fitMoS2PeaksL()
+            else:
+                runList[-1].fitMoS2PeaksG()
 #            df = pd.DataFrame[\
 #            ({'X':runList[-1].xpos, 'Y':runList[-1].ypos, 'H1':runList[-1].out.params['g1_height'],\
 #            'fwhm1':runList[-1].out.params['g1_fwhm'],'l1':runList[-1].out.params['g1_center'],\
 #            'H2':runList[-1].out.params['g2_height'],\
 #            'fwhm2':runList[-1].out.params['g2_fwhm'],'l2':runList[-1].out.params['g2_center']\
 #            })
+            num+=1
     return runList, toDF(runList)
     
 def toDF(classList):
@@ -58,7 +66,14 @@ def toDF(classList):
     num+=1
     return master
     
-def plotHeatmaps(df):
+def plotHeatmaps(df, layers):
+    layerDict = {'1L':[384.7,402.8], '2L':[383.3,405.5], \
+    '3L':[383.2,406.5],'4L':[382.9,407.4], 'Bulk':[383,408]}    
+    
+    p1 = layerDict[layers][0]
+    p2 = layerDict[layers][1]
+    d = p2-p1
+    
 #    data = data[((data.l2-data.l1)>15)]
 #    data = data[((data.l2-data.l1)<30)]
 #    fig = plt.figure()
@@ -69,14 +84,15 @@ def plotHeatmaps(df):
     split = df.pivot(index = 'X', columns = 'Y', values = 'split')
     peak1 = df.pivot(index = 'X', columns = 'Y', values = 'l1')
     peak2 = df.pivot(index = 'X', columns = 'Y', values = 'l2')
-    sns.heatmap(peak1, vmin = 380, vmax = 386)
+    sns.heatmap(peak1, vmin = p1-4, vmax =p1+4,cmap = 'RdBu_r')
     plt.title('e1')
     plt.show()
-    sns.heatmap(peak2, vmin = 399, vmax = 410)
+    sns.heatmap(peak2, vmin = p2-4, vmax = p2+4,cmap = 'RdBu_r')
     plt.title('g1')
     plt.show()
-    sns.heatmap(split, vmin = 15, vmax = 25)
+    sns.heatmap(split, vmin = d-8, vmax = d+8,cmap = 'RdBu_r')
     plt.title('delta')
-    plt.show()
-    
+    plt.show()    
+layerDict = {'1L':[384.7,402.8], '2L':[383.3,405.5], \
+'3L':[383.2,406.5],'4L':[382.9,407.4], 'Bulk':[383,408]}
 
